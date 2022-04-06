90f24d4377248dc4e86219cbad7e92f3a67a9c3a
ryancardoso <ryanferreiracardoso864@gmail.com>
2022-04-06 00:46:48 -0300

    update icons data

--- src/components/atoms/icons/svg/index.js
+++ src/components/atoms/icons/svg/index.js
@@ -3,11 +3,23 @@ import burgerMenu from './burgerMenu.svg'
 import add from './add.svg'
 import template from './template.svg'
 
+/* const imagensPath = require.context('../svg', true, /\.svg$/)
+let images = {}
+imagensPath.keys().forEach(item => {
+  images[item.replace('./', '').replace('.svg', '')] = item
+})
+console.log(images) */
+
 const reqSvgs = require.context('../svg', true, /\.svg$/)
 const paths = reqSvgs.keys()
 
 const svgs = paths.map(path => reqSvgs(path))
-console.log('teste', svgs)
+const svgList = svgs.map(item => item.default.src)
+
+svgList.reduce((prev, next) => {
+  const key = next.slice(20).split('.')[0]
+  return { ...prev, [key]: next }
+}, {})
 
 export const svg = {
   burgerMenu,
