302850f3d9e10836aa5def999f3f6cffba31843a
Rianfiore <rianfiore@gmail.com>
2022-04-09 00:51:22 -0300

    instantiating list on Home Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,24 @@
 // Libs
-import styled from 'styled-components'
-import Icon from '../components/atoms/icons'
-
-const Title = styled.h1``
+import List from 'components/molecules/list'
+import { useState } from 'react'
 
 export default function Home() {
+  const [theme, setTheme] = useState('light')
   return (
     <>
-      <h1>Olá mundo</h1>
+      <div
+        style={{
+          width: '100%',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '100px',
+          background: theme === 'light' ? 'lightGray' : '#2E3844',
+          padding: '10px'
+        }}
+      >
+        <List title="Backlog do Projeto" theme={theme}></List>
+      </div>
     </>
   )
 }
